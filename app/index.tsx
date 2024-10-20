51ded8fd4d82dc4706027f2b7a8fb8409387fbdd
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-20 21:41:08 +0000

    chore/ setting things up

--- app/index.tsx
+++ app/index.tsx
@@ -1,27 +1,14 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Text, View } from "react-native";
 import React from "react";
 import { Link } from "expo-router";
 
 const Index = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World</Text>
-      <Link
-        href={"/profile"}
-        style={{ color: "blue", textDecorationStyle: "underline" }}
-      >
-        go to Profile page
-      </Link>
+    <View className="flex-1 items-center justify-center bg-red-600">
+      <Text className="text-3xl text-yellow-300">Hello World</Text>
+      <Link href={"/profile"}>go to Profile page</Link>
     </View>
   );
 };
 
 export default Index;
-
-const styles = StyleSheet.create({
-  container: {
-    alignItems: "center",
-    flex: 1,
-    justifyContent: "center",
-  },
-});
