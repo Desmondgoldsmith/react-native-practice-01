648e8072ce608ecf6eced5cd5c8f8a8114dfe741
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-21 03:07:25 +0000

    chore/ Working on the Sigi-in screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,11 +1,28 @@
-import { View, Text } from "react-native";
+import { View, Text, ScrollView, Image } from "react-native";
 import React from "react";
+import { SafeAreaView } from "react-native";
+import { images } from "../../constants";
+import Form from "../../components/formField";
 
 const SignIn = () => {
   return (
-    <View>
-      <Text>SignIn</Text>
-    </View>
+    <SafeAreaView className="bg-primary h-full">
+      <ScrollView>
+        <View className="px-4 py-6 w-full justify-center h-full">
+          <Image
+            source={images.logo}
+            className="w-[115px] h-[35px]"
+            resizeMode="contain"
+          />
+
+          <Text className="font-psemibold text-white mt-10 text-2xl text-semibold">
+            Log in to Aora
+          </Text>
+
+          <Form />
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
