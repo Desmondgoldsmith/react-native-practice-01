72f52102d9152e68e96db33c74066d1cd2cd839a
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-21 01:13:15 +0000

    chore/ done with the tab layout

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -52,7 +52,53 @@ const TabLayout: React.FC = () => {
           ),
         }}
       />
-      {/* Add other tab screens here */}
+      {/* bookmark screen */}
+      <Tabs.Screen
+        name="bookmark"
+        options={{
+          headerShown: false,
+          title: "Bookmark",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.bookmark}
+              color={color}
+              name={"Bookmark"}
+              focused={focused}
+            />
+          ),
+        }}
+      />
+      <Tabs.Screen
+        name="create"
+        options={{
+          headerShown: false,
+          title: "Create",
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              icon={icons.plus}
+              color={color}
+              name="Create"
+              focused={focused}
+            />
+          ),
+        }}
+      />
+
+      <Tabs.Screen
+        name="profile"
+        options={{
+          title: "Profile",
+          headerShown: false,
+          tabBarIcon: ({ color, focused }) => (
+            <TabIcon
+              name="Profile"
+              icon={icons.profile}
+              color={color}
+              focused={focused}
+            />
+          ),
+        }}
+      />
     </Tabs>
   );
 };
