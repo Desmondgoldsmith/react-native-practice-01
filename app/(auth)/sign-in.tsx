484eb44227a579f588297875f0506eeb6fafc5ed
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-21 03:20:53 +0000

    chore/ Working on the Sigi-in screen

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,10 +1,14 @@
 import { View, Text, ScrollView, Image } from "react-native";
-import React from "react";
+import React, { useState } from "react";
 import { SafeAreaView } from "react-native";
 import { images } from "../../constants";
 import Form from "../../components/formField";
 
 const SignIn = () => {
+  const [formData, setFormData] = useState({
+    username: "",
+    email: "",
+  });
   return (
     <SafeAreaView className="bg-primary h-full">
       <ScrollView>
@@ -18,8 +22,26 @@ const SignIn = () => {
           <Text className="font-psemibold text-white mt-10 text-2xl text-semibold">
             Log in to Aora
           </Text>
+          {/* email */}
+          <Form
+            title="Email"
+            value={formData.email}
+            handleChangeText={(event) =>
+              setFormData({ ...formData, email: event })
+            }
+            otherStyles="mt-7"
+            keyboardType="email-address"
+          />
 
-          <Form />
+          {/* password */}
+          <Form
+            title="Password"
+            value={formData.passwword}
+            handleChangeText={(event) =>
+              setFormData({ ...formData, password: event })
+            }
+            otherStyles="mt-7"
+          />
         </View>
       </ScrollView>
     </SafeAreaView>
--- components/formField.tsx
+++ components/formField.tsx
@@ -1,7 +1,8 @@
 import { View, Text } from "react-native";
 import React from "react";
 
-const Form = () => {
+
+const Form = ({ title, placeholder, handleChangeText value, otherStyles, ...props}) => {
   return (
     <View className="space-y-2">
       <Text className={`font-psemibold text-base text-gray-100`}>Form</Text>
