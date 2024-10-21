1d6b1291f323badc0f77cef73fc9f09a1a60ec74
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-21 03:30:47 +0000

    chore/ Working on the Sigi-in screen

--- components/formField.tsx
+++ components/formField.tsx
@@ -1,11 +1,29 @@
 import { View, Text } from "react-native";
 import React from "react";
 
+interface FormProps {
+  title: string;
+  placeholder: string;
+  handleChangeText: () => void;
+  value: string;
+  otherStyles: string;
+}
 
-const Form = ({ title, placeholder, handleChangeText value, otherStyles, ...props}) => {
+const Form: React.FC<FormProps> = ({
+  title,
+  placeholder,
+  handleChangeText,
+  value,
+  otherStyles,
+  ...props
+}) => {
   return (
-    <View className="space-y-2">
-      <Text className={`font-psemibold text-base text-gray-100`}>Form</Text>
+    <View className={`space-y-2 ${otherStyles}`}>
+      <Text className={`font-psemibold text-base text-gray-100`}>{title}</Text>
+
+      <View
+        className={`rounded-md bg-black-100 border border-black-200 w-full h-16`}
+      ></View>
     </View>
   );
 };
