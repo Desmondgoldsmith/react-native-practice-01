cd072d3b5b9bbe86098eeeeee61af44e90588f9f
Desmond Goldsmith <desmondgoldsmith07@gmail.com>
2024-10-29 03:36:41 +0000

    chore: working on the db setup

---
+++ context/Provider.tsx
@@ -0,0 +1,12 @@
+import { createContext, useContext, useState, useEffect } from "react";
+
+const GlobalContext = createContext();
+export const useGlobalContext = () => useContext(GlobalContext);
+
+const GlobalProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [user, setUser] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+
+  return <GlobalContext.Provider></GlobalContext.Provider>;
+};
